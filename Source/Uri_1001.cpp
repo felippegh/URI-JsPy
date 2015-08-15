4e59aa807dbc32ff6f6b91e39da12021f2ab05bc
Felippe G Haeitmann <felippegeorge@utexas.edu>
2015-08-15 04:09:35 -0500

    Added solution 1001

---
+++ Source/Uri_1001.cpp
@@ -0,0 +1,22 @@
+/*
+ *Solved by: Felippe George Haeitmann
+ *Email: felippegeorge@utexas.edu
+ *Problem source: urionlinejudge.com.br
+ *
+ 
+**/
+#include<stdio.h>
+#include<stdlib.h>
+ 
+int a,b,x;
+ 
+int main(){
+    scanf("%d",&a);
+    scanf("%d",&b);
+ 
+    x = a + b;
+ 
+    printf("X = %d\n",x);
+ 
+return 0;
+}
