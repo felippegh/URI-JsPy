84760ba3d4f454255e2a2798a5dcb7cf816713f3
Felippe G Haeitmann <felippegeorge@utexas.edu>
2015-08-16 04:30:54 -0500

    Solution 1003

---
+++ Source/Uri_1003.cpp
@@ -0,0 +1,21 @@
+/*
+ *Solved by: Felippe George Haeitmann
+ *Email: felippegeorge@utexas.edu
+ *Problem source: urionlinejudge.com.br
+ *
+ 
+**/
+
+#include<stdio.h>
+#include<stdlib.h>
+ 
+int main(){
+ 
+    int f,s,soma;
+ 
+    scanf(" %d %d",&f,&s);
+    soma = f + s;
+    printf("SOMA = %d\n",soma);
+ 
+    return 0;
+}
