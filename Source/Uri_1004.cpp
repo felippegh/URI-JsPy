8f51a0b123846267bd3e81f398aaac210df1ae1f
Felippe G Haeitmann <felippegeorge@utexas.edu>
2015-08-16 04:31:09 -0500

    Solution 1004

---
+++ Source/Uri_1004.cpp
@@ -0,0 +1,21 @@
+/*
+ *Solved by: Felippe George Haeitmann
+ *Email: felippegeorge@utexas.edu
+ *Problem source: urionlinejudge.com.br
+ *
+ 
+**/
+
+#include<stdio.h>
+#include<stdlib.h>
+ 
+int main(){
+ 
+    int f,s,prod;
+ 
+    scanf(" %d %d",&f,&s);
+    prod = f * s;
+    printf("PROD = %d\n",prod);
+ 
+    return 0;
+}
